1c7c424a16458c9227b358427c990c38fddc00cc
Yuri Gladkov <cyberglad@mail.ru>
2020-03-14 22:31:11 +0100

    Initial Commit
    Created main Gradle project

---
+++ src/main/java/com/example/springwebfluxlinkshortener/SpringWebfluxLinkShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxlinkshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxLinkShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxLinkShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwebfluxlinkshortener/SpringWebfluxLinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxlinkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxLinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
